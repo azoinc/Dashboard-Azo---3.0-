f15024c39d07d88ffb0ccfe7d173d21d0ef577d0
Bruno Mossato <bhmossato@gmail.com>
2026-03-18 12:43:18 -0300

    Update Firebase config to use only environment variables
    Removed default values for Firebase configuration.

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAT_Lw5NJ3l0pL3yIvHJUwO9l_d8xqgrGk",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "azo-dash-3-0.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "azo-dash-3-0",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "azo-dash-3-0.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "841923046209",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:841923046209:web:07fd3bb8d2eb7e10dd4fc0",
-  measurementId: "G-B37R0BYH3H"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+ 
 };
 
 // Initialize Firebase
