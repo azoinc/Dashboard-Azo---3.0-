8e5acd757a99688a43784ebf1e3dfa7127ea3978
Bruno Mossato <bhmossato@gmail.com>
2026-05-08 13:20:54 -0300

    Update InternoDashboard.tsx

--- src/pages/InternoDashboard.tsx
+++ src/pages/InternoDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import { useAuth } from '../context/AuthContext';
 import { LogOut, ArrowLeft, BarChart3, Users, Megaphone, Filter, ChevronDown, Check } from 'lucide-react';
 import { 
